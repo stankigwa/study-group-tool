0f4bccdda893957d5e56c10a4945354ce631ba54
Kigwa Stan <stanwaiganjo60@gmail.com>
2025-04-10 20:26:52 +0300

    Updated MongoDB URI, ignored .env, ready for deployment

--- server.js
+++ server.js
@@ -27,10 +27,7 @@ app.use(cors(corsOptions)); // Use CORS with options
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ Database Connection Error:", err));
 
